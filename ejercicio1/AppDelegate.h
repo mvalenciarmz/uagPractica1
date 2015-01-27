0eb94f7ecf888b90d7ef21783395e0e69e52386a
Marcos <marcos@Marcoss-Mac-mini.local>
2015-01-26 19:10:51 -0800

    Initial Commit

---
+++ ejercicio1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ejercicio1
+//
+//  Created by Marcos on 1/26/15.
+//  Copyright (c) 2015 Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
